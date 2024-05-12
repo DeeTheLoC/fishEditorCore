32387f09219d4eb34dcae255c5d4d6110f79133a
DeeLoC <michael951386@my.yosemite.edu>
2024-05-12 04:05:50 -0700

    improve

--- assets/resources/bzier/scripts/Bezier.js
+++ assets/resources/bzier/scripts/Bezier.js
@@ -1,92 +1,96 @@
 const Bezier = function (pointArr, allTime = 2) {
+  if (!Array.isArray(pointArr) || pointArr.length < 2) {
+    throw new Error("pointArr must be a non-empty array");
+  }
 
-    let _this = {}
-    // 曲线点集合,曲线总长,上一个点,当前时间
-    let _pointLists, totalLength, prevPos, currentRunTime;
-    // 运行时间
-    let _runTime = allTime;
-    let _pointArr = pointArr;
-    // 重置数据
-    let resetData = function () {
-        // 点集合
-        _pointLists = [];
-        // 线段总长度
-        totalLength = currentRunTime = 0;
-        // 初始位置
-        prevPos = {
-            x: pointArr[0].x,
-            y: pointArr[0].y,
-            length: 0,
-        }
+  if (typeof allTime !== "number" || allTime <= 0) {
+    throw new Error("allTime must be a positive number");
+  }
+
+  let _this = {};
+  let _pointLists = [];
+  let totalLength = 0;
+  let prevPos = { x: pointArr[0].x, y: pointArr[0].y, length: 0 };
+  let currentRunTime = 0;
+  let runTime = allTime;
+  let pointArrLength = pointArr.length;
+
+  const factorial = (i) => {
+    if (i < 0) return 0;
+    if (i === 0 || i === 1) return 1;
+    return Array.from({ length: i }, (_, k) => k + 1).reduce((acc, val) => acc * val, 1);
+  };
+
+  const ComputeBezier = (dt, runTime) => {
+    if (pointArrLength < 2) {
+      throw new Error("pointArr must have at least two elements");
     }
 
-    //阶乘
-    function factorial(i) {
-        let n = 1;
-        for (let j = 1; j <= i; j++)
-            n *= j;
-        return n;
-    }
-    // ------------------------【核心代码】---------------------------
-    let ComputeBezier = function (dt, runTime) {
-        // 把时间从 [0,runTime] 映射到 [0,1] 之间
-        let t = currentRunTime / runTime;
+    let t = currentRunTime / runTime;
 
-        var x = 0, y = 0;
-        //控制点数组
-        var n = _pointArr.length - 1;
+    const x = pointArr.reduce((acc, item, index) => {
+      if (index === 0) {
+        return acc + item.x * Math.pow((1 - t), pointArrLength - index - 1) * Math.pow(t, index);
+      }
 
-        _pointArr.forEach((item, index) => {
-            if (!index) {
-                x += item.x * Math.pow((1 - t), n - index) * Math.pow(t, index)
-                y += item.y * Math.pow((1 - t), n - index) * Math.pow(t, index)
-            } else {
-                //factorial为阶乘函数
-                x += factorial(n) / factorial(index) / factorial(n - index) * item.x * Math.pow((1 - t), n - index) * Math.pow(t, index)
-                y += factorial(n) / factorial(index) / factorial(n - index) * item.y * Math.pow((1 - t), n - index) * Math.pow(t, index)
-            }
-        })
+      return (
+        acc +
+        (factorial(pointArrLength) / (factorial(index) * factorial(pointArrLength - index - 1))) *
+          item.x *
+          Math.pow((1 - t), pointArrLength - index - 1) *
+          Math.pow(t, index)
+      );
+    }, 0);
 
-        //  // 二阶贝塞尔曲线公式 (t => [0,1]) 
-        //  var x = Math.pow(1 - t, 2) * _startPos.x
-        //  + 2 * t * (1 - t) * _controlPos.x
-        //  + Math.pow(t, 2) * _endPos.x;
-         
-        //  var y = Math.pow(1 - t, 2) * _startPos.y
-        //  + 2 * t * (1 - t) * _controlPos.y
-        //  + Math.pow(t, 2) * _endPos.y;
+    const y = pointArr.reduce((acc, item, index) => {
+      if (index === 0) {
+        return acc + item.y * Math.pow((1 - t), pointArrLength - index - 1) * Math.pow(t, index);
+      }
 
-        // console.log(`x:${x},y:${y}`);
-        // 计算两点距离
-        let length = Math.sqrt(Math.pow(prevPos.x - x, 2) + Math.pow(prevPos.y - y, 2));
-        let v2 = { x, y, length };
-        // 存储当前节点
-        _pointLists.push(v2);
-        prevPos = v2;
-        // 累计长度
-        totalLength += length;
-        // 累计时间
-        currentRunTime += dt;
+      return (
+        acc +
+        (factorial(pointArrLength) / (factorial(index) * factorial(pointArrLength - index - 1))) *
+          item.y *
+          Math.pow((1 - t), pointArrLength - index - 1) *
+          Math.pow(t, index)
+      );
+    }, 0);
 
+    const length = Math.sqrt(Math.pow(prevPos.x - x, 2) + Math.pow(prevPos.y - y, 2));
+    const v2 = { x, y, length };
+
+    _pointLists.push(v2);
+    prevPos = v2;
+    totalLength += length;
+    currentRunTime += dt;
+  };
+
+  _this.getPoints = (count = 200) => {
+    resetData();
+    const dt = runTime / count;
+
+    for (let i = 0, len = count + 1; i < len; i++) {
+      ComputeBezier(dt, runTime);
     }
 
-    // 切割贝塞尔曲线
-    _this.getPoints = function (count = 200) {
-        resetData();
-        // 分割时间
-        let dt = _runTime / count;
-        // 开始分割曲线
-        for (var i = 0, len = count + 1; i < len; i++) {
-            ComputeBezier(dt, _runTime);
-        }
-        return _pointLists
-    }
+    return _pointLists;
+  };
 
-    _this.getCurveLength = function () {
-        return totalLength;
-    }
+  _this.getCurveLength = () => {
+    return totalLength;
+  };
 
-    return _this;
-}
+  const resetData = () => {
+    _pointLists = [];
+    totalLength = currentRunTime = 0;
+    prevPos = {
+      x: pointArr[0].x,
+      y: pointArr[0].y,
+      length: 0,
+    };
+  };
+
+  return _this;
+};
 
 module.exports = Bezier;
