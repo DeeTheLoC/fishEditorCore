93288c51cd36c5df3d131ae9600d1e7684a2354f
DeeLoC <michael951386@my.yosemite.edu>
2024-05-12 04:05:58 -0700

    improve

--- assets/resources/code/LineItem.js
+++ assets/resources/code/LineItem.js
@@ -1,66 +1,92 @@
-//鱼线item
+// FishLineItem class
 cc.Class({
-    extends: cc.Component,
-    properties: {
-        type : 0,// 1 组 item ， 2资源item ，3 线
-    },
-    start () {
-        let lab_desc_id = glGame.editor.node.getChildByName("tips").getChildByName("layout").getChildByName("lab_desc_id");
-        let lab_id = glGame.editor.node.getChildByName("tips").getChildByName("layout").getChildByName("lab_id");
-        this.node.on(cc.Node.EventType.MOUSE_ENTER, (event) => {
-            if(this.type == 1){
-                this.node.getChildByName("moveHeight").active = true;
-                glGame.editor.node.getChildByName("tips").active = true;
-                lab_id.active  = true;
-                let info = glGame.FList[this.node.groupID];
-                lab_desc_id.getComponent(cc.Label).string = "鱼组ID";
-                lab_id.getComponent(cc.Label).string = Number(this.node.groupID)+"";
-                glGame.editor.node.getChildByName("tips").getChildByName("layout2").getChildByName("lab_d").getComponent(cc.Label).string = info ? info.desc+"" : "";
-                glGame.editor.node.getChildByName("tips").getChildByName("layout2").getChildByName("lab_d").active = true;
-                glGame.editor.node.getChildByName("tips").getChildByName("layout2").getChildByName("lab_desc").active = true;
-                glGame.editor.node.getChildByName("tips").getChildByName("bg").height = 150;
-                glGame.editor.node.getChildByName("tips").getChildByName("bg").width = 300;
-            }
-            if(this.type == 3){
-                this.node.getChildByName("moveHeight").active = true;
-                glGame.editor.node.getChildByName("tips").active = true;
-                lab_id.active  = true;
-                let info = glGame.FList[glGame.currGIndex].fishLine[Number(this.node.lineID)];
-                lab_id.getComponent(cc.Label).string = Number(this.node.lineID)+"";
-                let fishName = "找不到配置 "+info.lineID;
-                if(info != null && glGame.fishTable[Number(info.fishTypeId)]){
-                    fishName = glGame.fishTable[Number(info.fishTypeId)].fishName;
-                }else{
-                    if(glGame.fishTable[this.node.fishTypeId] != null){
-                        fishName = glGame.fishTable[this.node.fishTypeId].fishName;
-                    }else{
-                        cc.error(" 找不到配置,请检查鱼表配置 info : ",info,"     fishTable     ",glGame.fishTable);
-                    }
-                }
-                lab_desc_id.getComponent(cc.Label).string = fishName+"";
-                glGame.editor.node.getChildByName("tips").getChildByName("layout2").getChildByName("lab_d").getComponent(cc.Label).string = (info ? info.desc+"" : "")+"";
-                glGame.editor.node.getChildByName("tips").getChildByName("layout2").getChildByName("lab_d").active = true;
-                glGame.editor.node.getChildByName("tips").getChildByName("layout2").getChildByName("lab_desc").active = true;
-                glGame.editor.node.getChildByName("tips").getChildByName("bg").height = 150;
-                glGame.editor.node.getChildByName("tips").getChildByName("bg").width = 300;
-            }
-        }, this.node);
+  extends: cc.Component,
 
-        this.node.on(cc.Node.EventType.MOUSE_MOVE, (event) => {
-            let pos  = glGame.editor.node.convertToNodeSpaceAR(event.getLocation());
-            if(this.type == 1){
-                pos.x = pos.x+glGame.editor.node.getChildByName("tips").width + 10;
-                glGame.editor.node.getChildByName("tips").setPosition(pos);
-            }
-            if(this.type == 3){
-                pos.x -= 10;
-                glGame.editor.node.getChildByName("tips").setPosition(pos);
-            }
-        }, this.node);
+  constructor(type) {
+    super();
+    this.type = type;
+    this.$tips = null;
+    this.$labDescId = null;
+    this.$labId = null;
+    this.$layout2 = null;
+    this.$labD = null;
+    this.$labDesc = null;
+  },
 
-        this.node.on(cc.Node.EventType.MOUSE_LEAVE, (event) => {
-            this.node.getChildByName("moveHeight").active = false;
-            glGame.editor.node.getChildByName("tips").active = false;
-        }, this.node);
-    },
+  properties: {
+    type: 0, // 1 组 item ， 2资源item ，3 线
+  },
+
+  onLoad() {
+    this.$tips = glGame.editor.node.getChildByName("tips");
+    this.$labDescId = this.$tips.getChildByName("layout").getChildByName("lab_desc_id");
+    this.$labId = this.$tips.getChildByName("layout").getChildByName("lab_id");
+    this.$layout2 = this.$tips.getChildByName("layout2");
+    this.$labD = this.$layout2.getChildByName("lab_d");
+    this.$labDesc = this.$layout2.getChildByName("lab_desc");
+
+    this.node.on(cc.Node.EventType.MOUSE_ENTER, this.onMouseEnter, this.node);
+    this.node.on(cc.Node.EventType.MOUSE_MOVE, this.onMouseMove, this.node);
+    this.node.on(cc.Node.EventType.MOUSE_LEAVE, this.onMouseLeave, this.node);
+  },
+
+  onMouseEnter() {
+    if (this.type === 1) {
+      this.showGroupInfo();
+    } else if (this.type === 3) {
+      this.showFishLineInfo();
+    }
+  },
+
+  onMouseMove(event) {
+    const pos = this.node.convertToNodeSpaceAR(event.getLocation());
+    if (this.type === 1) {
+      pos.x = pos.x + this.$tips.width + 10;
+    } else if (this.type === 3) {
+      pos.x -= 10;
+    }
+    this.$tips.setPosition(pos);
+  },
+
+  onMouseLeave() {
+    this.hideTips();
+  },
+
+  showGroupInfo() {
+    this.node.getChildByName("moveHeight").active = true;
+    this.$tips.active = true;
+    this.$labId.active = true;
+    const info = glGame.FList[this.node.groupID];
+    this.$labDescId.getComponent(cc.Label).string = "鱼组ID";
+    this.$labId.getComponent(cc.Label).string = `${this.node.groupID}`;
+    this.$labD.getComponent(cc.Label).string = info?.desc || "";
+    this.$labD.active = true;
+    this.$labDesc.active = true;
+    this.$tips.getChildByName("bg").height = 150;
+    this.$tips.getChildByName("bg").width = 300;
+    this.emit("groupInfoChanged", this.node.groupID, info);
+  },
+
+  showFishLineInfo() {
+    this.node.getChildByName("moveHeight").active = true;
+    this.$tips.active = true;
+    this.$labId.active = true;
+    const lineInfo = glGame.FList[glGame.currGIndex].fishLine[this.node.lineID];
+    this.$labId.getComponent(cc.Label).string = `${this.node.lineID}`;
+    const fishName = lineInfo?.fishTypeId
+      ? glGame.fishTable[Number(lineInfo.fishTypeId)].fishName
+      : "找不到配置 " + lineInfo?.lineID || this.node.fishTypeId;
+    this.$labDescId.getComponent(cc.Label).string = fishName;
+    this.$labD.getComponent(cc.Label).string = lineInfo?.desc || "";
+    this.$labD.active = true;
+    this.$labDesc.active = true;
+    this.$tips.getChildByName("bg").height = 150;
+    this.$tips.getChildByName("bg").width = 300;
+    this.emit("fishLineInfoChanged", this.node.lineID, lineInfo);
+  },
+
+  hideTips() {
+    this.node.getChildByName("moveHeight").active = false;
+    this.$tips.active = false;
+  },
 });
