c5dfdb6da94f820729d3acb81c587a083ed47b33
DeeLoC <michael951386@my.yosemite.edu>
2024-05-12 04:05:41 -0700

    improve

--- assets/resources/code/Drag.js
+++ assets/resources/code/Drag.js
@@ -4,33 +4,35 @@ cc.Class({
     properties: {
         isSelf: {
             default: false,
-            displayName: "拖自己or父类",
-            tooltip: "拖动自己 true 父类 false",
-            type: cc.boolean,
+            displayName: "Drag self or parent",
+            tooltip: "Drag self: true, parent: false",
+            type: cc.Boolean,
         },
         isEnterCtrl: {
             default: false,
-            displayName: "是否要按下Ctrl",
-            tooltip: " true 需要 false不需要",
-            type: cc.boolean,
+            displayName: "Require Ctrl key press",
+            tooltip: "True: require Ctrl key press, false: no requirement",
+            type: cc.Boolean,
         },
     },
-    start () {
+
+    start() {
         this.node.on(cc.Node.EventType.TOUCH_MOVE, (event) => {
             this.opacity = 255;
-            if(this.node.parent.getComponent(cc.Widget)){
-                this.node.parent.getComponent(cc.Widget).enabled = false;
+            const widget = this.node.parent.getComponent(cc.Widget);
+            if (widget) {
+                widget.enabled = false;
             }
-            var delta = event.touch.getDelta();
-            if(this.isEnterCtrl){
-                if(!glGame.Ctrl){
+            const delta = event.touch.getDelta();
+            if (this.isEnterCtrl) {
+                if (typeof glGame.Ctrl === 'undefined' || !glGame.Ctrl) {
                     return;
                 }
             }
-            if(this.isSelf){
+            if (this.isSelf) {
                 this.node.x += delta.x;
                 this.node.y += delta.y;
-            }else{
+            } else {
                 this.node.parent.x += delta.x;
                 this.node.parent.y += delta.y;
             }
